fee9d5053177026ed8b45756cc9d2568f80504f4
novel <novelbafagih@student.ub.ac.id>
2021-12-02 13:02:38 +0700

    uncomment updateProfile

--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,7 @@ $router->group(['prefix' => 'api'], function () use ($router) {
         $router->post('/logout', 'AuthController@logout');
         $router->get('/me', 'AuthController@me');
         $router->get('/like', 'RecipeLikeController@getLikeRecipes');
-        // $router->patch('/me', 'AuthController@updateProfile');
+        $router->patch('/me', 'AuthController@updateProfile');
         $router->group(['prefix' => 'recipe'], function () use ($router) {
             $router->post('/', 'RecipeController@createRecipe');
             $router->patch('/{id}', 'RecipeController@changeMyRecipe');
